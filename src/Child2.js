5234757a0e0c67a4f91ed6bb34806740769342be
Muhammad Anas Baig <anasbaigmughal@gmail.com>
2020-07-19 20:38:18 +0500

    Update Child2.js

--- src/Child2.js
+++ src/Child2.js
@@ -7,8 +7,8 @@ function Child2() {
         <div>
             Child 2 {state}
             <br /><br />
-            <button onClick={() => {dispatch({type:"INCREMENT", masg:10});}}>Increment</button>
-            <button onClick={() => {dispatch({type:"DECREMENT", masg:20});}}>Decrement</button>
+            <button onClick={() => { dispatch({ type: "INCREMENT", masg: 10 }); }}>Increment</button>
+            <button onClick={() => { dispatch({ type: "DECREMENT", masg: 20 }); }}>Decrement</button>
         </div>
     );
 }
